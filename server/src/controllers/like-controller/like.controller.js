1322e6bc0163117946a263a25e269fe6401bdb1f
amuhammed005 <amuhammed005@st.ug.edu.gh>
2026-04-16 04:24:03 +0000

    feat(like): implement and tested like system functionality

--- server/src/controllers/like-controller/like.controller.js
+++ server/src/controllers/like-controller/like.controller.js
@@ -1,4 +1,13 @@
+import { likeService } from "../../services/like-service/like.service.js";
+import { handleHttpError } from "../../utils/Error.handler.js";
+
 export const toggleLike = async (req, res) => {
   try {
-  } catch (error) {}
+    const { postId } = req.params;
+    const userId = req.user.userId;
+    const result = await likeService(postId, userId);
+    return res.json(result);
+  } catch (error) {
+    return handleHttpError(res, error);
+  }
 };
--- server/src/services/like-service/like.service.js
+++ server/src/services/like-service/like.service.js
@@ -1,5 +1,53 @@
-const likeService = async () => {
+import { prisma } from "../../lib/prisma.js";
+import { createValidationError } from "../../utils/Error.handler.js";
 
-}
+const likeService = async (postId, userId) => {
+  if (!postId || typeof postId !== "string" || !userId) {
+    throw createValidationError("User ID and Post ID are required");
+  }
 
-return (likeService)
+  const post = await prisma.post.findUnique({
+    where: { id: postId },
+  });
+  if (!post) {
+    throw createValidationError("Post not found");
+  }
+
+  const existingLike = await prisma.like.findUnique({
+    where: {
+      postId_userId: {
+        postId,
+        userId,
+      },
+    },
+  });
+  let liked = false;
+  let message = "";
+  if (existingLike) {
+    await prisma.like.delete({
+      where: {
+        postId_userId: {
+          postId,
+          userId,
+        },
+      },
+    });
+    message = "Unlike post";
+    liked = false;
+  } else {
+    await prisma.like.create({
+      data: {
+        postId,
+        userId,
+      },
+    });
+    message = "Liked post";
+    liked = true;
+  }
+  return {
+    message,
+    liked,
+  };
+};
+
+export { likeService };
